754c3ac0fba7b64820fa1ba4168e24017e65bf6d
Larkaholic <orillosdominique@gmail.com>
2025-01-25 20:47:24 +0800

    working blade

---
+++ resources/views/greet.blade.php
@@ -0,0 +1,10 @@
+<!DOCTYPE html>
+<html>
+<head>
+    <title>Greet Page</title>
+</head>
+<body>
+    <h1>Welcome to My Laravel App</h1>
+    <p>This is a greeting from the GreetController!</p>
+</body>
+</html>
